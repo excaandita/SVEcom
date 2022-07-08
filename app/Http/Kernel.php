54fd5af5300ab0e7899ee4c554dc4e398f77ae84
Hafidh Sajid <hafidhsajid@gmail.com>
2022-07-09 01:42:40 +0700

    middleware route seller

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\IsAdmin::class,
         'buyer' => \App\Http\Middleware\IsBuyer::class,
+        'seller' => \App\Http\Middleware\IsSeller::class,
     ];
 }
