08cda1548ee7ddc18586728dec10545d9de0bc71
Hafidh Sajid <hafidhsajid@gmail.com>
2022-07-12 20:12:03 +0700

    seller controller

--- app/Http/Controllers/Seller/DashboardController.php
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -6,10 +6,8 @@ use App\Models\User;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
-
-
-
+use App\Models\TransactionDetail;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -18,11 +16,23 @@ class DashboardController extends Controller
         $customer = User::count();
         $revenue = Transaction::sum('total_price');
         $transaction = Transaction::count();
-
+        $pending = Transaction::where('transaction_status', 'PENDING')->count();
+        $success = Transaction::where('transaction_status', 'SUCCESS')->count();
+        $canceled = Transaction::where('transaction_status', 'CANCELLED')->count();
+        $recentlytransaction=Transaction::orderBy('transactions.updated_at', 'desc')->join('users','users.id','transactions.users_id')->limit(3)->get();
+        // $bestselling = DB::select("SELECT *, (select count(*) from transaction_details where products.id = transaction_details.products_id) AS count FROM products ORDER BY count DESC");
+        $bestselling = DB::select("SELECT *,products.name as nama_produk, categories.name as nama_kategori, (select count(*) from transaction_details where products.id = transaction_details.products_id) AS count FROM products JOIN categories ON products.categories_id = categories.id
+        ORDER BY count DESC;");
+        // dd($bestselling);
         return view('pages.seller.dashboard', [
             'customer' => $customer,
             'revenue' => $revenue,
             'transaction' => $transaction,
+            'pending' => $pending,
+            'success' => $success,
+            'canceled' => $canceled,
+            'recentlytransaction'=>$recentlytransaction,
+            'bestselling'=>$bestselling,
 
         ]);
     }
