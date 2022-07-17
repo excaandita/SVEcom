ddfc8d1bc5e7e76ab245a08b643139b9a6f19645
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-07-18 00:43:21 +0700

    fix: migration

--- database/migrations/2022_07_17_172301_add_is_public_to_users_table.php
+++ database/migrations/2022_07_17_172301_add_is_public_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsPublicToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('isPublic');
+            $table->boolean('isPublic')->default(0);
         });
     }
 
