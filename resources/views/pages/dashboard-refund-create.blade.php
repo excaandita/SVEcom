50c5ca71571f11d3743a118e0a85840f701af453
Exca Muchlis Andita <excamuchlisandita@gmail.com>
2022-07-18 21:51:11 +0700

    refund-repair

--- resources/views/pages/dashboard-refund-create.blade.php
+++ resources/views/pages/dashboard-refund-create.blade.php
@@ -34,7 +34,7 @@
                                     <div class="col-md-12">
                                         <div class="form-group">
                                           <label>Kode Transaksi</label>
-                                          <input type="text" name="transaction_details_id" class="form-control" value="{{ $codeTransactions->code }}" required>
+                                          <input type="text" class="form-control" value="{{ $codeTransactions->code }}" disabled required>
                                         </div>
                                     </div>
                                     <div class="col-md-12">
@@ -49,7 +49,7 @@
                                     <div class="col-md-12">
                                       <div class="form-group">
                                           <label>Total Penarikan</label>
-                                          <input type="number" name="total" class="form-control" value="{{ $codeTransactions->transaction->total_price }}" required>
+                                          <input type="number" class="form-control" value="{{ $codeTransactions->transaction->total_price }}" disabled required>
                                       </div>
                                     </div>
                                     <div class="col-md-12">
@@ -73,6 +73,8 @@
                                       </div>
                                     </div>
                                     <input type="hidden" name="users_id" value="{{ Auth::user()->id }}">
+                                    <input type="hidden" name="transaction_details_id" value="{{ $codeTransactions->code }}">
+                                    <input type="hidden" name="total" value="{{ $codeTransactions->transaction->total_price }}">
                                     <input type="hidden" name="status" value="PENDING">
                                 </div>
                                 <div class="row">
