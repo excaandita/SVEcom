76d68940b0b19f7f16652727fdf4ef3a2268995c
rizqunazs <rizqunasania@gmail.com>
2022-08-09 12:24:47 +0700

    ubah fe

--- resources/views/pages/listproduct.blade.php
+++ resources/views/pages/listproduct.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app1')
 
 @section('title')
+<<<<<<< Updated upstream
     Kategori - Sekolah Vokasi E-COM
+=======
+   Produk - Sekolah Vokasi E-COM
+>>>>>>> Stashed changes
 @endsection
 
 @section('content')
@@ -23,6 +27,7 @@
                 </div>
               </form>
             </div>
+<<<<<<< Updated upstream
         </div>
         <div class="container">
       <div class="row">
@@ -43,6 +48,38 @@
                 <div class="products-thumbnail">
                   <div
                     class="products-image"
+=======
+          </div>
+          <div class="col-lg-9">
+            <div class="shop__product__option">
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="shop__product__option__left">
+                     
+                    <p>Showing 1–9 of {{ $products->count()}}</p>
+                    
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="shop__product__option__right">
+                    <p>Sort by Price:</p>
+                    <select>
+                      <option value="">Low To High</option>
+                      <option value="">$0 - $55</option>
+                      <option value="">$55 - $100</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+                @forelse ($products as $product )
+              <div class="col-lg-4 col-md-4 col-sm-6" data-aos="fade-up">
+               <div class="product__item">
+                 <div class="image-content-list">
+                   <div
+                    class="product__item__pic"
+>>>>>>> Stashed changes
                     style="
                       @if($product->galleries->count())
                         background-image: url('{{ Storage::url($product->galleries->first()->photos) }}')
