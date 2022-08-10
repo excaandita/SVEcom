c5d30347a13a4ec1e77e10e9510ffddc54bfac66
excaandita <76029874+excaandita@users.noreply.github.com>
2022-08-10 12:05:31 +0700

    Revert "Edit02"

--- resources/views/pages/listproduct.blade.php
+++ resources/views/pages/listproduct.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-   Produk - Sekolah Vokasi E-COM
+    Cart - Sekolah Vokasi E-COM
 @endsection
 @section('content')
     <!-- Breadcrumb Section Begin -->
@@ -19,32 +19,91 @@
                 </div>
             </div>
         </div>
-        <div class="container">
-      <div class="row">
-        <div class="col-12" data-aos="fade-up">
-          <h5>Semua Produk</h5>
-        </div>
-      </div>
-      <div class="row">
-      <!-- batas new Product-->
-        @php $incrementProduct = 0 @endphp
-          @forelse ($products as $product )
-            <div
-            class="col-4 col-md-4 col-lg-3 mt-3"
-            data-aos="fade-up"
-            data-aos-delay="{{ $incrementProduct+= 100 }}"
-            >
-              <a href="{{ route('detail', $product->slug)}}" class="component-products d-block">
-                <div class="products-thumbnail">
-                  <div
-                    class="products-image"
+    </section>
+    <!-- Shop Section Begin -->
+    <section class="shop spad">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="shop__sidebar">
+                <div class="shop__sidebar__search">
+                <form action="{{ route('listproduct') }}"  method="GET">
+                    
+                        <input type="search" name="search" value="{{ request()->get('search') }}" class="form-control form-control-lg"  placeholder="Search..." />
+                        <div class="input-group-append">
+                            <button type="submit">
+                            <span class="fa fa-search"></span>
+                        </button>
+                        </div>
+                </form>
+              </div>
+              <div class="shop__sidebar__accordion">
+                <div class="accordion" id="accordionExample">
+                  <div class="card">
+                    <div class="card-heading">
+                      <a data-toggle="collapse" data-target="#collapseOne"
+                        >Categories</a
+                      >
+                    </div>
+                    <div
+                      id="collapseOne"
+                      class="collapse show"
+                      data-parent="#accordionExample"
+                    >
+                      <div class="card-body">
+                        <div class="shop__sidebar__categories">
+                          <ul class="nice-scroll">
+                            @foreach ($categories as $category)
+                            <li><a href="{{ route('product-categories', $category->slug) }}">{{ $category->name }}</a></li>
+                            @endforeach
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card">
+                    
+                    <div
+                      id="collapseThree"
+                      class="collapse show"
+                      data-parent="#accordionExample"
+                    >
+                    </div>
+                  </div>
+                  <div class="card">
+                    <div class="card-heading">
+                      <a data-toggle="collapse" data-target="#collapseSix"
+                        >Tags</a
+                      >
+                    </div>
+                    <div
+                      id="collapseSix"
+                      class="collapse show"
+                      data-parent="#accordionExample"
+                    >
+                      <div class="card-body">
+                        <div class="shop__sidebar__tags">
+                          <a href="#">Product</a>
+                          <a href="#">Bags</a>
+                          <a href="#">Shoes</a>
+                          <a href="#">Fashio</a>
+                          <a href="#">Clothing</a>
+                          <a href="#">Hats</a>
+                          <a href="#">Accessories</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-lg-9">
             <div class="shop__product__option">
               <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-6">
                   <div class="shop__product__option__left">
-                    <p>Showing 1–9 of {{ $products->count()}}</p>
+                    <p>Showing 1–12 of 126 results</p>
                   </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
@@ -61,9 +120,9 @@
             </div>
             <div class="row">
                 @forelse ($products as $product )
-              <div class="col-lg-4 col-md-4 col-sm-6" data-aos="fade-up">
+              <div class="col-lg-4 col-md-4 col-sm-6">
                <div class="product__item">
-                 <div class="image-content-list">
+                 <div class="image-content">
                    <div
                     class="product__item__pic"
                     style="
