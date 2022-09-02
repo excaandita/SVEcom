534bea2d3a5de4d3c0b0efe2ed90529df305c285
zahrazaelani <zahrazaelani@student.uns.ac.id>
2022-09-02 15:38:56 +0700

    typo

--- resources/views/pages/dashboard-account.blade.php
+++ resources/views/pages/dashboard-account.blade.php
@@ -11,7 +11,7 @@
       <div class="section-header">
         <h1>Seller Dashboard - My Account</h1>
         <div class="section-header-breadcrumb">
-          <div class="breadcrumb-item active">Update yout current profile</div>
+          <div class="breadcrumb-item active">Update your current profile</div>
         </div>
       </div>
       <div class="dashboard-content">
